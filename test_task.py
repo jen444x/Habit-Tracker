88bfe09e29b92d613b0db8f7c69b1583281231ea
jen444x <106173914+jen444x@users.noreply.github.com>
2025-11-10 17:52:00 -0800

    Create tests using pytest

---
+++ test_task.py
@@ -0,0 +1,26 @@
+from task import Task
+
+def test_instantiation_with_only_name_provided():
+    """Test that objects are instantiated correctly when only task name is provided"""
+    task_name = 'Task1'
+    task = Task(task_name)
+    
+    # Get values
+    task_dict = task.__dict__
+    correct_dict = {'name': task_name, 'description': '', 'due_date': ''}
+    
+    # compare
+    assert task_dict == correct_dict
+    
+def test_instantiation_with_only_name_description_provided():
+    """Test that objects are instantiated correctly when only task name is provided"""
+    task_name = 'Task1'
+    task_desc = 'Complete Task1'
+    task = Task(task_name, task_desc)
+    
+    # Get values
+    task_dict = task.__dict__
+    correct_dict = {'name': task_name, 'description': task_desc, 'due_date': ''}
+    
+    # compare
+    assert task_dict == correct_dict
