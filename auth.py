31b614377c187ac242d668cb89ebc36842e24577
jen444x <106173914+jen444x@users.noreply.github.com>
2025-11-22 15:07:12 -0800

    Complete auth
    - Set up /logout route
    - added ftn to run when request is made to any auth view to check if user is stored in session
    - decorator to make sure user is logged in before hitting certain endpoints

--- auth.py
+++ auth.py
@@ -76,4 +76,33 @@ def login():
 
     return render_template('auth/login.html')
 
+# always run when a request is made to the bp url
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        db = get_db()
+        user = db.execute(
+            'SELECT * FROM user WHERE id = ?', (user_id)
+        ).fetchone()
+
+        g.user = user
+        
+@bp.route('/logout')
+def logout():
+    session.clear
+    return redirect(url_for('index'))
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        
+        return view(**kwargs)
+    
+    return wrapped_view
 
