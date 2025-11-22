d3b10798cc91a65242d3fe4866c997d2055494ab
jen444x <106173914+jen444x@users.noreply.github.com>
2025-11-22 13:53:53 -0800

    Set up /auth

---
+++ auth.py
@@ -0,0 +1,41 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from habit_tracke.db import get_db
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+@bp.route('register', methods=('GET', 'POST'))
+def register():
+    if request.method =='POST': 
+        # Get data
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        # Make sure both fields were entered
+        if not username or not password:
+            error = "Both fields are required."
+
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
+                    (username, generate_password_hash(password))
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"User {username} is already registered."
+            else:
+                return redirect(url_for("auth.login"))
+            
+        flash(error)
+
+    return render_template('auth/register.html')
+
